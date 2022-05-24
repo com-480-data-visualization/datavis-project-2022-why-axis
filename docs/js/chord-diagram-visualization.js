b6f48343f548e010aea3b7e2f47cb20e6e6b90ec
Maciej Styczen <styczenm1@gmail.com>
2022-05-24 10:58:24 +0200

    Initial version of chord (no labels)

--- docs/js/chord-diagram-visualization.js
+++ docs/js/chord-diagram-visualization.js
@@ -1,44 +1,65 @@
-// create the svg area
-const chord_diagram = d3.select("#chord-diagram").append("g").attr("transform", "translate(300, 300)")
+const json_path = "data/viz2.json"
+const colors = ['#FF6633', '#FFB399', '#FF33FF', '#FFFF99', '#00B3E6',
+		  '#E6B333', '#3366E6', '#999966', '#99FF99', '#B34D4D',
+		  '#80B300', '#809900', '#E6B3B3', '#6680B3', '#66991A',
+		  '#FF99E6', '#CCFF1A', '#FF1A66', '#E6331A', '#33FFCC',
+		  '#66994D', '#B366CC', '#4D8000', '#B33300', '#CC80CC',
+		  '#66664D', '#991AFF', '#E666FF', '#4DB3FF', '#1AB399',
+		  '#E666B3', '#33991A', '#CC9999', '#B3B31A', '#00E680',
+		  '#4D8066', '#809980', '#E6FF80', '#1AFF33', '#999933',
+		  '#FF3380', '#CCCC00', '#66E64D', '#4D80CC', '#9900B3',
+		  '#E64D66', '#4DB380', '#FF4D4D', '#99E6E6', '#6666FF'];
+const innerRadius = 360
+const outerRadius = 380
 
-// create input data: a square matrix that provides flow between entities
-const matrix = [
-    [11975, 5871, 8916, 2868],
-    [1951, 10048, 2060, 6171],
-    [8010, 16145, 8090, 8045],
-    [1013, 990, 940, 6907]
-];
+$.getJSON(json_path, function(data) {
+    const matrix = data["matrix"]
+    const labels = data["labels"]
 
-// give this matrix to d3.chord(): it will calculate all the info we need to draw arc and ribbon
-const res = d3.chord()
-    .padAngle(0.05)     // padding between entities (black arc)
+    chordDiagram(matrix, labels)
+});
+
+function chordDiagram(matrix, labels) {
+    // create the svg area
+    const chordArea = document.getElementById("chord-diagram")
+    const height = chordArea.getAttribute("height")
+    const width = chordArea.getAttribute("width")
+    console.log(chordArea)
+    const chord_diagram = d3.select("#chord-diagram").append("g").attr("transform", `translate(${height/2},${width/2})`)
+    const res = d3.chord()
+    .padAngle(0.01)     // padding between entities (black arc)
     .sortSubgroups(d3.descending)
     (matrix)
 
-// add the groups on the inner part of the circle
-chord_diagram
-    .datum(res)
-    .append("g")
-    .selectAll("g")
-    .data(d => d.groups)
-    .join("g")
-    .append("path")
-    .style("fill", "grey")
-    .style("stroke", "black")
-    .attr("d", d3.arc()
-        .innerRadius(200)
-        .outerRadius(210)
-    )
+    // based on https://d3-graph-gallery.com/graph/chord_basic.html
+    // add the groups on the inner part of the circle
+    chord_diagram
+        .datum(res)
+        .append("g")
+        .selectAll("g")
+        .data(d => d.groups)
+        .join("g")
+        .append("path")
+        .style("fill", "grey")
+        .style("stroke", "black")
+        .attr("d", d3.arc()
+            .innerRadius(innerRadius)
+            .outerRadius(outerRadius)
+        )
 
-// Add the links between groups
-chord_diagram
-    .datum(res)
-    .append("g")
-    .selectAll("path")
-    .data(d => d)
-    .join("path")
-    .attr("d", d3.ribbon()
-        .radius(200)
-    )
-    .style("fill", "#69b3a2")
-    .style("stroke", "black");
+    chord_diagram.append("path")
+      .attr("fill", "none")
+      .attr("d", d3.arc()({outerRadius, startAngle: 0, endAngle: 2 * Math.PI}));
+
+    // // Add the links between groups
+    chord_diagram
+        .datum(res)
+        .append("g")
+        .selectAll("path")
+        .data(d => d)
+        .join("path")
+        .attr("d", d3.ribbon()
+            .radius(innerRadius - 10)
+        )
+        .style("fill", function(d){ return(colors[d.source.index]) })
+}

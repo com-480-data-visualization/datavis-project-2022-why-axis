2642754165bf6d6e904283e357f57c1812b2e0ea
NDresevic <nevena.dresevic17@gmail.com>
2022-06-01 13:17:33 +0200

    updating colors in map, added dummy slider

--- docs/js/map-visualization.js
+++ docs/js/map-visualization.js
@@ -14,35 +14,72 @@ function map(data) {
     // Data and color scale
     const erasmus_map_data = new Map();
 
-    // const colors = d3.scaleThreshold()
-    // .domain([10000,100000,500000,1000000,5000000,10000000,50000000,100000000,500000000,1500000000])
-    // // .range(["rgb(247,251,255)", "rgb(222,235,247)", "rgb(198,219,239)", "rgb(158,202,225)", "rgb(107,174,214)", "rgb(66,146,198)","rgb(33,113,181)","rgb(8,81,156)","rgb(8,48,107)","rgb(3,19,43)"]);
-    // .range(d3.schemeBlues[7]);
-    // const erasmus_map_colorScale = d3.scaleOrdinal().range(colors);
+    document.getElementById('cb-female').addEventListener('change', _ => updateColors());
+    document.getElementById('cb-male').addEventListener('change', _ => updateColors());
+    document.getElementById('cb-sending').addEventListener('change', _ => updateColors());
+    document.getElementById('cb-receiving').addEventListener('change', _ => updateColors());
 
-    const erasmus_map_colorScale = d3.scaleThreshold()
-        .domain([100000, 1000000, 10000000, 30000000, 100000000, 500000000])
-        .range(d3.schemeBlues[7]);
+    function updateColors() {
+        // todo: handle year
+        var year = "";
+        const cb_female = $("#cb-female").is(":checked");
+        const cb_male = $("#cb-male").is(":checked");
+        const cb_sending = $("#cb-sending").is(":checked");
+        const cb_receiving = $("#cb-receiving").is(":checked");
 
-    // var sliderTime = d3.sliderBottom()
-    //     .min(d3.min(dataTime))
-    //     .max(d3.max(dataTime))
-    //     .step(1000 * 60 * 60 * 24 * 365)
-    //     .width(300)
-    //     .tickFormat(d3.timeFormat('%Y'))
-    //     .tickValues(dataTime)
-    //     .default(new Date(1998, 10, 3))
-    //     .on('onchange', val => {
-    //         d3.select('p#value-time').text(d3.timeFormat('%Y')(val));
-    //     });
+        var maxCount = 0;
+        var participants = 0
+        var countryParticipants = new Map();
+        for (var i = 0; i < data.length; i++) {
+            var obj = data[i];
+            year = obj["Yearly-Data"][0]["Academic Year"];
+            if (cb_sending && cb_receiving && cb_female && cb_male) participants = obj["Yearly-Data"][0]["All-All"];
+            else if (cb_sending && cb_receiving && cb_male) participants = obj["Yearly-Data"][0]["All-Male"];
+            else if (cb_sending && cb_receiving & cb_female) participants = obj["Yearly-Data"][0]["All-Female"];
+            else if (cb_receiving && cb_female && cb_male) participants = obj["Yearly-Data"][0]["Receiving-All"];
+            else if (cb_sending && cb_female && cb_male) participants = obj["Yearly-Data"][0]["Sending-All"];
+            else if (cb_sending && cb_female) participants = obj["Yearly-Data"][0]["Sending-Female"];
+            else if (cb_sending && cb_male) participants = obj["Yearly-Data"][0]["Sending-Male"];
+            else if (cb_receiving & cb_female) participants = obj["Yearly-Data"][0]["Receiving-Female"];
+            else if (cb_receiving && cb_male) participants = obj["Yearly-Data"][0]["Receiving-Male"];
+            if (participants > maxCount) maxCount = participants;
+            countryParticipants.set(obj["Country"], participants);
+        }
+        for (const entry of countryParticipants.entries()) {
+            const text = "[country_name=\'" + entry[0] + "\']"
+            var country = $("#erasmus-map").find(text)[0]
+            if (country != undefined) country.style.fill = d3.interpolateRdBu((0.5 + (entry[1] / maxCount) / 2));
+        }
+    }
 
-    // var gTime = d3.select('div#slider-time')
-    //     .append('svg')
-    //     .attr('width', 500)
-    //     .attr('height', 100)
-    //     .append('g')
-    //     .attr('transform', 'translate(30,30)');
+    var sliderData = [1, 2, 3, 4, 5, 6];
 
+    // Step
+    var sliderStep = d3
+        .sliderBottom()
+        .min(d3.min(sliderData))
+        .max(d3.max(sliderData))
+        .width(300)
+        .tickFormat(d3.format("d"))
+        .ticks(6)
+        .step(1)
+        .default(sliderData[0])
+        .on('onchange', val => {
+            d3.select('p#value-step').text(d3.format("d")(val));
+        });
+
+    var gStep = d3
+        .select('div#slider-step')
+        .append('svg')
+        .attr('width', 500)
+        .attr('height', 100)
+        .append('g')
+        .attr('transform', 'translate(30,30)');
+
+    gStep.call(sliderStep);
+
+    d3.select('p#value-step').text(d3.format("d")(sliderStep.value()));
+    
     // Load external data and boot
     Promise.all([
         d3.json("https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/world.geojson"),
@@ -54,12 +91,8 @@ function map(data) {
             // Tooltip stuff
             var tooltip = d3.select("#tooltip-map")
 
-            // // Slider stuff --> https://bl.ocks.org/johnwalley/e1d256b81e51da68f7feb632a53c3518
-            // gTime.call(sliderTime);
-            // d3.select('p#value-time').text(d3.timeFormat('%Y')(sliderTime.value()));
-
             let mouseOver = function (d) {
-                tooltip.style("visibility", "visible");
+                tooltip.style("visibility", "visible").transition()
 
                 d3.selectAll(".Country")
                     .transition()
@@ -73,7 +106,7 @@ function map(data) {
             }
 
             let mouseLeave = function (d) {
-                tooltip.style("visibility", "hidden");
+                tooltip.style("visibility", "hidden").transition()
 
                 d3.selectAll(".Country")
                     .transition()
@@ -87,14 +120,6 @@ function map(data) {
 
             let mouseMove = function (d) {
                 const name = d.path[0].__data__["properties"]["name"]
-                var obj = null;
-                for (var i = 0; i < data.length; i++) {
-                    if (data[i]["Country"] == name) {
-                        obj = data[i];
-                        console.log(obj);
-                        break;
-                    }
-                }
 
                 // todo: handle year
                 var year = "";
@@ -106,10 +131,14 @@ function map(data) {
                 const cb_receiving = $("#cb-receiving").is(":checked");
                 var gender = (cb_female && cb_male) ? "Female, Male" : ((cb_female ? "Female" : (cb_male ? "Male" : "")))
                 var program = (cb_sending && cb_receiving) ? "Sending, Receiving" : ((cb_sending ? "Sending" : (cb_receiving ? "Receiving" : "")))
-                var participants = 0
 
+                var obj = null;
+                var maxCount = 0;
+                var participants = 0
                 var text = "No data for Erasmus programs."
-                if (obj != null) {
+                for (var i = 0; i < data.length; i++) {
+                    var obj = data[i];
+
                     year = obj["Yearly-Data"][0]["Academic Year"];
 
                     if (cb_sending && cb_receiving && cb_female && cb_male) participants = obj["Yearly-Data"][0]["All-All"];
@@ -122,11 +151,19 @@ function map(data) {
                     else if (cb_receiving & cb_female) participants = obj["Yearly-Data"][0]["Receiving-Female"];
                     else if (cb_receiving && cb_male) participants = obj["Yearly-Data"][0]["Receiving-Male"];
 
-                    text = "Academic Year: " + year + "<br>" + "Gender: " + gender + "<br>" + "Program Type: " + program + "<br>" + "Participants: " + participants;
+                    if (participants > maxCount) maxCount = participants;
+
+                    if (obj["Country"] == name) {
+                        text = "Academic Year: " + year + "<br>" + "Gender: " + gender + "<br>" + "Program Type: " + program + "<br>" + "Participants: " + participants;
+                        break;
+                    }
                 }
+                // todo: fix size of box
                 tooltip.html("Country: " + name + "<br>" + text)
                     .style("left", (d3.pointer(d)[0] - erasmus_map_width / 2) + "px")
                     .style("top", (d3.pointer(d)[1]) + "px")
+                    .transition()
+                // d.srcElement.style.fill = d3.interpolateRdBu((0.5 + (participants / maxCount) / 2));
             }
 
             // Draw the map
@@ -141,11 +178,20 @@ function map(data) {
                 )
                 // set the color of each country
                 .attr("fill", function (d) {
-                    d.total = erasmus_map_data.get(d.id) || 0;
-                    return erasmus_map_colorScale(d.total);
+                    var maxCount = 0;
+                    var participants = 0;
+                    for (var i = 0; i < data.length; i++) {
+                        var obj = data[i];
+                        if (obj["Yearly-Data"][0]["All-All"] > maxCount) maxCount = obj["Yearly-Data"][0]["All-All"];
+                        if (d.properties.name == obj["Country"]) {
+                            participants = obj["Yearly-Data"][0]["All-All"];
+                        }
+                    }
+                    return d3.interpolateRdBu(0.5 + (participants / maxCount) / 2);
                 })
                 .style("stroke", "transparent")
                 .attr("class", function (d) { return "Country" })
+                .attr("country_name", function (d) { return d.properties.name })
                 .style("opacity", .8)
                 .on("mouseover", mouseOver)
                 .on("mouseleave", mouseLeave)
